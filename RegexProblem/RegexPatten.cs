0abf425e28e4065e9ca2ad3005f91f2d3a0b1173
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-03-06 10:25:07 +0530

    [Rushi]:Exception for Null and Empty input

--- RegexProblem/RegexPatten.cs
+++ RegexProblem/RegexPatten.cs
@@ -8,31 +8,50 @@ namespace RegexProblem
         public string ValidateNameFirstName(string firstName)
         {
             string NamePatten = "^[A-Z][a-z]{3,}?";
-
-            if (Regex.IsMatch(firstName, NamePatten))
+            try
             {
-                Console.WriteLine("First Name is Valid");
-                return "First Name is Valid";
-            }
-            else 
+                if (firstName.Equals(string.Empty))
+                {
+                    throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionType.NULL_INPUT, "First Name should not be Empty");
+                }
+                if (Regex.IsMatch(firstName, NamePatten))
+                {
+                    Console.WriteLine("First Name is Valid");
+                    return "First Name is Valid";
+                }
+                else
+                {
+                    Console.WriteLine("First Name not Valid");
+                    return "First Name is not Valid";
+                }
+            }catch (NullReferenceException)
             {
-                Console.WriteLine("First Name not Valid");
-                return "First Name is not Valid";
+                throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionType.NULL_INPUT, "First Name should not be Null");
             }
         }
-        public string ValidateNameLastName( string lastName)
+        public string ValidateNameLastName(string lastName)
         {
             string NamePatten = "^[A-Z][a-z]{3,}?";
-
-            if (Regex.IsMatch(lastName, NamePatten))
+            try
             {
-                Console.WriteLine("Last Name is Valid");
-                return "Last Name is Valid";
+                if (lastName.Equals(string.Empty))
+                {
+                    throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionType.EMPTY_INPUT, "Last Name should not be Empty");
+                }
+                if (Regex.IsMatch(lastName, NamePatten))
+                {
+                    Console.WriteLine("Last Name is Valid");
+                    return "Last Name is Valid";
+                }
+                else
+                {
+                    Console.WriteLine("Last Name is not Valid ");
+                    return "Last Name is not Valid";
+                }
             }
-            else
+            catch(NullReferenceException) 
             {
-                Console.WriteLine("Last Name is not Valid ");
-                return "Last Name is not Valid";
+                throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionType.NULL_INPUT, "Last Name should not be Null");
             }
         }
 
@@ -40,31 +59,52 @@ namespace RegexProblem
         {
             //abc.xyz@bl.co.in
             string emailPatten = "^[a-zA-Z]+[.+_-]{0,1}[a-z]+[@][a-zA-Z]+[.][a-z]{2,3}([.][a-z]{2}){0,1}$";
-
-            if (Regex.IsMatch(email, emailPatten))
+            try
             {
-                Console.WriteLine("Email is Valid");
-                return "Email is Valid";
+                if (email.Equals(string.Empty))
+                {
+                    throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionType.EMPTY_INPUT, "Email should not be Empty");
+                }
+                if (Regex.IsMatch(email, emailPatten))
+                {
+                    Console.WriteLine("Email is Valid");
+                    return "Email is Valid";
+                }
+                else
+                {
+                    Console.WriteLine("Email not Valid ");
+                    return "Email is Valid";
+                }
             }
-            else
+            catch(NullReferenceException)
             {
-                Console.WriteLine("Email not Valid ");
-                return "Email is Valid";
+                throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionType.NULL_INPUT, "Email should not be Null");
             }
         }
 
         public string ValidateMobileNumber(string mobileNumber)
         {
             string mobileNumberPatten = @"[0-9]{1,2}[ ][0-9]{10}"; /*@"[0-9]{1,2}\s[0-9]{10}"*/
-            if (Regex.IsMatch(mobileNumber, mobileNumberPatten))
+            try
             {
-                Console.WriteLine("Mobile Number is Valid");
-                return "Mobile Number is Valid";
+                if (mobileNumber.Equals(string.Empty))
+                {
+                    throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionType.EMPTY_INPUT, "Mobile Number should not be Empty");
+                }
+                if (Regex.IsMatch(mobileNumber, mobileNumberPatten))
+                {
+                    Console.WriteLine("Mobile Number is Valid");
+                    return "Mobile Number is Valid";
+                }
+                else
+                {
+                    Console.WriteLine("Mobile Number not Valid");
+                    return "Mobile Number not Valid";
+                }
             }
-            else
+            catch(NullReferenceException)
             {
-                Console.WriteLine("Mobile Number not Valid");
-                return "Mobile Number not Valid";
+                throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionType.NULL_INPUT, "Mobile Number should not be Null");
             }
         }
 

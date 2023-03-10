7403b642c7a77141cdaca836db0d580b615cd9a9
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-03-10 16:42:45 +0530

    [Rushi]:Refractor user registration to Lambda function

---
+++ UserRegistrationLamda/RegistrationLambda.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UserRegistrationLamda
+{
+    public class RegistrationLambda
+    {
+        List<RegistrationModel> registrationPattenList = new List<RegistrationModel>();
+        public RegistrationLambda()
+        {
+            RegistrationModel registration = new RegistrationModel();
+            registrationPattenList.Add(registration);
+        }
+        public void CheckFirstName(string firstName)
+        {
+            if (registrationPattenList.Any(x => x.FirstNamePatten.IsMatch(firstName)))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+        public void CheckLastName(string lastName)
+        {
+            if (registrationPattenList.Any(x => x.LastNamePatten.IsMatch(lastName)))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+
+        public void CheckEmail(string eMail)
+        {
+            if (registrationPattenList.Any(x => x.EmailPatten.IsMatch(eMail)))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+
+        public void CheckobileNum(string mobileNum)
+        {
+            if (registrationPattenList.Any(x => x.MobileNumPatten.IsMatch(mobileNum)))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+
+        public void CheckPassword1(string password)
+        {
+            if (registrationPattenList.Any(x => x.Password1Patten.IsMatch(password)))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+        public void CheckPassword2(string password)
+        {
+            if (registrationPattenList.Any(x => x.Password2Patten.IsMatch(password)))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+
+        public void CheckPassword3(string password)
+        {
+            if (registrationPattenList.Any(x => x.Password3Patten.IsMatch(password)))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+
+        public void CheckPassword4(string password)
+        {
+            if (registrationPattenList.Any(x => x.Password4Patten.IsMatch(password)))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+
+}
---
+++ UserRegistrationLamda/RegistrationModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRegistrationLamda
+{
+    public class RegistrationModel
+    {
+        public Regex FirstNamePatten = new Regex(@"^[A-Z][a-z]{3,}?");
+        public Regex LastNamePatten = new Regex(@"^[A-Z][a-z]{3,}?");
+        public Regex EmailPatten = new Regex(@"^[a-zA-Z]+[.+_-]{0,1}[a-z]+[@][a-zA-Z]+[.][a-z]{2,3}([.][a-z]{2}){0,1}$");
+        public Regex MobileNumPatten = new Regex(@"[0-9]{1,2}[ ][0-9]{10}");
+        public Regex Password1Patten = new Regex(@"[A-Z a-z 0-9]{8,}");
+        public Regex Password2Patten = new Regex(@"(?=.*[A-Z])[A-Za-z0-9]{8,}");
+        public Regex Password3Patten = new Regex(@"(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}");
+        public Regex Password4Patten = new Regex(@"""^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#$%^&*()_+=-]).{8,}$""");
+    }
+}
